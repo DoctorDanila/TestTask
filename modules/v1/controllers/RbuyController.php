ce2139ef4624800226149ee50cc44219fea912f3
DoctorDanila <50800526+DoctorDanila@users.noreply.github.com>
2024-03-16 21:48:44 +0500

    {controllers} feature: add limitation to controllers
    get param: limit=<limit_num>
    example: limit=500

--- modules/v1/controllers/RbuyController.php
+++ modules/v1/controllers/RbuyController.php
@@ -9,4 +9,19 @@ class RbuyController extends BaseAPIController
 {
     public $modelClass = \app\modules\v1\models\RestateBuyInfo::class;
 
+    public function actions(){
+        $actions = parent::actions();
+        unset($actions['index']);
+        return $actions;
+    }
+    public function actionIndex(){
+        $activeData = new ActiveDataProvider([
+            'query' => \app\modules\v1\models\RestateBuyInfo::find(),
+            'pagination' => [
+                'defaultPageSize' => \Yii::$app->request->get('limit', 20),
+                'pageSizeLimit' => [1, 500],
+            ],
+        ]);
+        return $activeData;
+    }
 }
