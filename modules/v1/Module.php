f025ad3dd85f1a5aad0c31c386c47e68e6096fff
DoctorDanila <50800526+DoctorDanila@users.noreply.github.com>
2024-03-16 22:01:26 +0500

    (Module) feature: generate Module v1

---
+++ modules/v1/Module.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\modules\v1;
+
+use app\modules\v1\models\Users;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\auth\QueryParamAuth;
+
+/**
+ * v1 module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\v1\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+
+}
