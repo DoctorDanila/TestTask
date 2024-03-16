8cc1cea1c683291b8bcf41219738f6a12027b6b6
DoctorDanila <50800526+DoctorDanila@users.noreply.github.com>
2024-03-16 21:38:26 +0500

    (BaseAPIController) feature:add serializer to BaseAPIController
    this is necessary in order to wrap the data in the response, as well as to output _meta and _link information

--- components/controllers/BaseAPIController.php
+++ components/controllers/BaseAPIController.php
@@ -7,5 +7,10 @@ use yii\rest\ActiveController;
 class BaseAPIController extends ActiveController
 {
 
+    public $serializer = [
+        'class'=>'yii\rest\Serializer',
+        'collectionEnvelope'=>'items',
+    ];
+
 
 }
