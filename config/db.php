481b2bfac23b1c3df2b33a50163438fd5f98bcc3
DoctorDanila <50800526+DoctorDanila@users.noreply.github.com>
2024-03-16 21:35:42 +0500

    {global} structure: install yii2 basic template

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=eternity-business.ru:3306;dbname=u2508379_default',
+    'charset' => 'utf8',
+    'enableQueryCache' => false,
+    'enableSchemaCache' => false,
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
